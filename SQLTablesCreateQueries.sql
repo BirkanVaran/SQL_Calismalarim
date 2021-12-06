977d529b43404079a5203f71b04784da782fb288
birkanvaranofficial@gmail.com <birkanvaranofficial@gmail.com>
2021-12-06 14:55:58 +0300

    Yeni SQL Queryler eklendi.

--- SQLTablesCreateQueries.sql
+++ SQLTablesCreateQueries.sql
@@ -3,3 +3,14 @@
 -- Yazarlar tablosu eklendi.
 
 -- Turler tablosu eklendi.
+
+-- İşlem tablosu oluşturuldu:
+
+CREATE TABLE Islem
+(
+IslemID int IDENTITY(1,1) PRIMARY KEY,
+OgrID int NOT NULL,
+KitapID int NOT NULL,
+OduncAldigiTarih datetime NOT NULL,
+OduncBitisTarih datetime NOT NULL
+)
